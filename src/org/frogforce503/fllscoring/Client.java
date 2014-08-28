5cc14d2dc289eb55e868e2e86fe6c1bb8975a024
Jacob Steffler <jacobsteffler@gmail.com>
2014-08-28 18:21:23 -0400

    Modified makefile and basic window for Client

--- src/org/frogforce503/fllscoring/Client.java
+++ src/org/frogforce503/fllscoring/Client.java
@@ -1,17 +1,28 @@
 package org.frogforce503.fllscoring;
 
+import java.awt.*;
 import javax.swing.*;
 
 public class Client implements Runnable {
+	//GUI element declarations
+	private JFrame frame;
+	private Container cp;
+	
 	public void run() {
-		
+		frame = new JFrame("Scores");
+		frame.setResizable(false);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		cp = frame.getContentPane();
+		cp.setPreferredSize(new Dimension(1024, 768));
+
+		frame.pack();
+		frame.setVisible(true);
 	}
 
 	public static void main(String[] args) {
 		Client client = new Client();
 		
 		SwingUtilities.invokeLater(client);
-
-
 	}
 }
