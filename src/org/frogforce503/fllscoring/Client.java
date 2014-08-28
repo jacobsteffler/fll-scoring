201f8224c5c21ecd7b0b601ec564575bbbe00b9d
Jacob Steffler <jacobsteffler@gmail.com>
2014-08-28 18:35:15 -0400

    Set Nimbus LAF

--- src/org/frogforce503/fllscoring/Client.java
+++ src/org/frogforce503/fllscoring/Client.java
@@ -2,18 +2,20 @@ package org.frogforce503.fllscoring;
 
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.UIManager.*;
 
 public class Client implements Runnable {
 	//GUI element declarations
 	private JFrame frame;
 	private Container cp;
-	
+
 	public void run() {
 		frame = new JFrame("Scores");
 		frame.setResizable(false);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		cp = frame.getContentPane();
+		cp.setLayout(null);
 		cp.setPreferredSize(new Dimension(1024, 768));
 
 		frame.pack();
@@ -21,8 +23,17 @@ public class Client implements Runnable {
 	}
 
 	public static void main(String[] args) {
+		try {
+			for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+				if ("Nimbus".equals(info.getName())) {
+					UIManager.setLookAndFeel(info.getClassName());
+					break;
+				}
+			}
+		} catch (Exception e) {}
+
 		Client client = new Client();
-		
+
 		SwingUtilities.invokeLater(client);
 	}
 }
