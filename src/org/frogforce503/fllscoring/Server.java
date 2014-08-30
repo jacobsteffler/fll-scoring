b87a646d0addb67c0b3779d5ebce765ef67b4bb7
Jacob Steffler <jacobsteffler@gmail.com>
2014-08-29 23:38:08 -0400

    Asking for port number, Server listening

--- src/org/frogforce503/fllscoring/Server.java
+++ src/org/frogforce503/fllscoring/Server.java
@@ -2,12 +2,22 @@ package org.frogforce503.fllscoring;
 
 import java.io.*;
 import java.net.*;
+import javax.swing.*;
 
 public class Server {
 	private ServerSocket ssock;
+	private int port = 0;
 
 	public Server() throws IOException {
-		ssock = new ServerSocket(41361);
+		//TODO Error checking
+		port = Integer.parseInt(JOptionPane.showInputDialog(null, "Please enter a port number:", "Port", JOptionPane.QUESTION_MESSAGE));
+
+		ssock = new ServerSocket(port);
+
+		Socket client;
+		while(true) {
+			client = ssock.accept();
+		}
 	}
 
 	public static void main(String[] args) {
@@ -15,6 +25,7 @@ public class Server {
 			new Server();
 		} catch(Exception e) {
 			e.printStackTrace();
+			System.exit(1);
 		}
 	}
 }
