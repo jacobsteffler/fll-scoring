d4015c26418d15edf722ab2de04c1c088fc0c907
Jacob Steffler <jacobsteffler@gmail.com>
2014-08-30 13:35:49 -0400

    Nimbus and error checking for port number

--- src/org/frogforce503/fllscoring/Server.java
+++ src/org/frogforce503/fllscoring/Server.java
@@ -3,16 +3,30 @@ package org.frogforce503.fllscoring;
 import java.io.*;
 import java.net.*;
 import javax.swing.*;
+import javax.swing.UIManager.*;
 
 public class Server {
-	private ServerSocket ssock;
+	private ServerSocket ssock = new ServerSocket();
 	private int port = 0;
 
 	public Server() throws IOException {
-		//TODO Error checking
-		port = Integer.parseInt(JOptionPane.showInputDialog(null, "Please enter a port number:", "Port", JOptionPane.QUESTION_MESSAGE));
+		while(!ssock.isBound()) {
+			while(port < 1 || port > 65535) {
+				String ports = (JOptionPane.showInputDialog(null, "Please enter a port number (1-65535):", "Port", JOptionPane.QUESTION_MESSAGE));
+				if(ports == null) System.exit(0);
+				try {
+					port = Integer.parseInt(ports);
+				} catch(NumberFormatException e) {}
+			}
 
-		ssock = new ServerSocket(port);
+
+			try {
+				ssock = new ServerSocket(port);
+			} catch(BindException e) {
+				JOptionPane.showMessageDialog(null, "Port " + port + " is not available. Please choose another.", "Bind error", JOptionPane.ERROR_MESSAGE);
+				port = 0;
+			}
+		}
 
 		Socket client;
 		while(true) {
@@ -21,6 +35,15 @@ public class Server {
 	}
 
 	public static void main(String[] args) {
+		try {
+			for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+				if ("Nimbus".equals(info.getName())) {
+					UIManager.setLookAndFeel(info.getClassName());
+					break;
+				}
+			}
+		} catch(Exception e) {}
+
 		try {
 			new Server();
 		} catch(Exception e) {
